c69e4589507d79b8c6581b0473bda9c6e0136a7f
Anton Simagin <simaginam@gmail.com>
2020-08-08 17:46:17 +0300

    Add project files.

---
+++ MovieLibrary/Models/MenuTreeViewContent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace MovieLibrary.Models
+{
+    public class MenuTreeViewContent
+    {
+        public List<TreeCategory> Content { get; }
+
+        public MenuTreeViewContent()
+        {
+            Content = new List<TreeCategory>
+            {
+                new TreeCategory("Жанр",
+                    new TreeItem("Детектив", "Crime", false),
+                    new TreeItem("Комедия", "Comedy", false),
+                    new TreeItem("Приключения", "Adventure", false),
+                    new TreeItem("Триллер", "Thriller", false),
+                    new TreeItem("Ужасы", "Horror", false),
+                    new TreeItem("Фантастика", "Sci-Fi", false),
+                    new TreeItem("Фэнтези", "Fantasy", false)),
+
+                new TreeCategory("Страна",
+                    new TreeItem("Австралия", "Australia", false),
+                    new TreeItem("Великобритания", "UK", false),
+                    new TreeItem("Германия", "Germany", false),
+                    new TreeItem("Испания", "Spain", false),
+                    new TreeItem("Италия", "Italy", false),
+                    new TreeItem("Канада", "Canada", false),
+                    new TreeItem("Мексика", "Mexico", false),
+                    new TreeItem("Россия", "Russia", false),
+                    new TreeItem("США", "USA", false),
+                    new TreeItem("Франция", "France", false),
+                    new TreeItem("Япония", "Japan", false))
+            };
+        }
+    }
+
+    public class TreeCategory
+    {
+        public string Name { get; }
+
+        public List<TreeItem> TreeItems { get; }
+
+        public TreeCategory(string name, params TreeItem[] items)
+        {
+            Name = name;
+            TreeItems = new List<TreeItem>(items);
+        }
+    }
+
+    public class TreeItem
+    {
+        public string Name { get; }
+        public string SearchParamName { get; }
+        public bool IsChecked { get; set; }
+
+        public TreeItem(string name, string searchParamName, bool isChecked)
+        {
+            Name = name;
+            SearchParamName = searchParamName;
+            IsChecked = isChecked;
+        }
+    }
+}
---
+++ MovieLibrary/ViewModel/MainViewModel.cs
@@ -0,0 +1,115 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MovieLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace MovieLibrary.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region Fields
+
+        public bool MenuToggleButtonIsChecked
+        {
+            get => _menuToggleButtonIsChecked;
+            set => Set(ref _menuToggleButtonIsChecked, value);
+        }
+        public bool ImdbIsChecked { get; set; }
+        public bool KinoPoiskIsChecked { get; set; }
+        public List<int> Dates { get; }
+        public int BeginYearSelected { get; set; }
+        public int EndYearSelected { get; set; }
+        public int RatingValue { get; set; }
+        public string ErrorText
+        {
+            get => _errorText;
+            set => Set(ref _errorText, value);
+        }
+        public List<TreeCategory> MenuTreeViewContent { get; }
+
+        private bool _menuToggleButtonIsChecked = false;
+        private string _errorText;
+        private MainWindow _window;
+        private ContentViewModel _contentViewModel;
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel(ContentViewModel contentViewModel)
+        {
+            _contentViewModel = contentViewModel;
+            _window = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+
+            MenuTreeViewContent = new MenuTreeViewContent().Content;
+            Dates = new List<int>(Enumerable.Range(1990, DateTime.Now.Year - 1989));
+            RatingValue = 5;
+            ImdbIsChecked = true;
+        }
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand ApplyParametersCommand => new RelayCommand(ApplyParameters);       
+        public RelayCommand LoadedProgramCommand => new RelayCommand(LoadContentView);
+        public RelayCommand<CancelEventArgs> ClosingProgramCommand => new RelayCommand<CancelEventArgs>(ClosingProgram);
+
+        private void ApplyParameters()
+        { 
+            List<TreeItem> genreItems = MenuTreeViewContent[0].TreeItems.Where(x => x.IsChecked).ToList();
+            List<TreeItem> countryItems = MenuTreeViewContent[1].TreeItems.Where(x => x.IsChecked).ToList();
+
+            if (genreItems.Count == 0)
+            {
+                ErrorText = "Выбирите жанр";
+            }
+            else if (BeginYearSelected == 0 && EndYearSelected == 0)
+            {
+                ErrorText = "Задайте временной период";
+            }
+            else if (BeginYearSelected == 0)
+            {
+                ErrorText = "Задайте\nминимальное значение\nвременного периода";
+            }
+            else if (EndYearSelected == 0)
+            {
+                ErrorText = "Задайте\nмаксимальное значение\nвременного периода";
+            }
+            else if (EndYearSelected < BeginYearSelected && EndYearSelected != 0)
+            {
+                ErrorText = "Минимальное значение\nвыбранного временного\nпериода не может быть\nбольше максимального";
+            }
+            else
+            {
+                MenuToggleButtonIsChecked = false;
+                ErrorText = string.Empty;
+            }
+        }
+        private void LoadContentView()
+        {
+            _window.MainPanel.Children.Clear();
+            _window.MainPanel.Children.Add(new View.ContentView() { DataContext = _contentViewModel });
+        }
+        private void ClosingProgram(CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Вы действительно хотите закрыть программу?", "Выход", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #endregion
+
+    }
+}
